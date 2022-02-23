10e436bd51f51a037eae504b1e9be3e294f1f84a
Duke Takle <duke.takle@gmail.com>
2022-02-22 20:15:18 -0600

    install the battery and Xsession scripts

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ fi
 
 ln -sf $(pwd)/dot_xinitrc ${HOME}/.xinitrc
 ln -sf $(pwd)/status.sh ${HOME}/bin/status.sh
+ln -sf $(pwd)/battery.sh ${HOME}/bin/battery.sh
+ln -sf $(pwd)/run_dwm.sh ${HOME}/bin/run_dwm.sh
 sudo pacman -S ttf-ubuntu-font-family ttf-font-awesome picom nitrogen xorg-xsetroot
