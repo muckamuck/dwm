8359a14b6edfc86aa9a61a34deb6d560848b9ef0
Duke Takle <duke.takle@protonmail.com>
2022-03-08 20:01:35 -0600

    start auto-mounting when running DWM

--- run_dwm.sh
+++ run_dwm.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ -f "$(which udiskie 2>/dev/null)" ]; then
+    udiskie 2>/tmp/udiskie.log > /tmp/udiskie.log &
+else
+    echo "For automount USB drives install udiskie udisks2" > /tmp/udiskie.log &
+fi
+
 ${HOME}/bin/status.sh &
 picom -f &
 nitrogen --restore &
